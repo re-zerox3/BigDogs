e556ec2cf5600548645036122f16941939a12460
re-zerox3 <50246622+re-zerox3@users.noreply.github.com>
2022-10-28 13:35:01 -0600

    Add files via upload
    Arduino File

---
+++ gpsModule.ino
@@ -0,0 +1,80 @@
+#include <TinyGPS++.h> // Include the TinyGPS++ library
+TinyGPSPlus tinyGPS; // Create a TinyGPSPlus object
+#define GPS_BAUD 9600 // GPS module baud rate. GP3906 defaults to 9600.
+#include <SoftwareSerial.h>
+#define ARDUINO_GPS_RX 9 // GPS TX, Arduino RX pin
+#define ARDUINO_GPS_TX 8 // GPS RX, Arduino TX pin
+SoftwareSerial ssGPS(ARDUINO_GPS_TX, ARDUINO_GPS_RX); // Create a SoftwareSerial
+#define gpsPort ssGPS  // Alternatively, use Serial1 on the Leonardo
+#define SerialMonitor Serial
+
+int LED = 13;
+
+void setup(){
+  SerialMonitor.begin(9600);
+  gpsPort.begin(GPS_BAUD);
+  pinMode(LED, OUTPUT);
+}
+
+void loop(){
+  // print position, altitude, speed, time/date, and satellites:
+  printGPSInfo();
+  digitalWrite(LED, HIGH);
+  smartDelay(1000); // "Smart delay" looks for GPS data while the Arduino's not doing anything else
+  digitalWrite(LED, LOW);
+}
+
+void printGPSInfo()
+{
+  // Print latitude, longitude, altitude in feet, course, speed, date, time,
+  // and the number of visible satellites.
+  SerialMonitor.print("Lat: "); SerialMonitor.println(tinyGPS.location.lat(), 6);
+  SerialMonitor.print("Long: "); SerialMonitor.println(tinyGPS.location.lng(), 6);
+  SerialMonitor.print("Alt: "); SerialMonitor.println(tinyGPS.altitude.feet());
+  SerialMonitor.print("Course: "); SerialMonitor.println(tinyGPS.course.deg());
+  SerialMonitor.print("Speed: "); SerialMonitor.println(tinyGPS.speed.mph());
+  SerialMonitor.print("Date: "); printDate();
+  SerialMonitor.print("Time: "); printTime();
+  SerialMonitor.print("Sats: "); SerialMonitor.println(tinyGPS.satellites.value());
+  SerialMonitor.println();
+}
+
+// This custom version of delay() ensures that the tinyGPS object
+// is being "fed". From the TinyGPS++ examples.
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do
+  {
+    // If data has come in from the GPS module
+    while (gpsPort.available())
+      tinyGPS.encode(gpsPort.read()); // Send it to the encode function
+    // tinyGPS.encode(char) continues to "load" the tinGPS object with new
+    // data coming in from the GPS module. As full NMEA strings begin to come in
+    // the tinyGPS library will be able to start parsing them for pertinent info
+  } while (millis() - start < ms);
+}
+
+// printDate() formats the date into dd/mm/yy.
+void printDate()
+{
+  SerialMonitor.print(tinyGPS.date.day());
+  SerialMonitor.print("/");
+  SerialMonitor.print(tinyGPS.date.month());
+  SerialMonitor.print("/");
+  SerialMonitor.println(tinyGPS.date.year());
+}
+
+// printTime() formats the time into "hh:mm:ss", and prints leading 0's
+// where they're called for.
+void printTime()
+{
+  SerialMonitor.print(tinyGPS.time.hour());
+  SerialMonitor.print(":");
+  if (tinyGPS.time.minute() < 10) SerialMonitor.print('0');
+  SerialMonitor.print(tinyGPS.time.minute());
+  SerialMonitor.print(":");
+  if (tinyGPS.time.second() < 10) SerialMonitor.print('0');
+  SerialMonitor.println(tinyGPS.time.second());
+}
+
